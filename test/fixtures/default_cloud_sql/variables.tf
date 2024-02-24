52336cfd44f3b63ce5c9c9f84e1d0d1ee1c2b99e
Brett Curtis <brett@osinfra.io>
2024-02-24 11:22:05 -0500

    Align tests and Infracost tags

--- test/fixtures/default_cloud_sql/variables.tf
+++ test/fixtures/default_cloud_sql/variables.tf
@@ -1,1 +1,22 @@
-../shared/variables.tf
+variable "client_certs" {
+  type = set(string)
+  default = [
+    "client-cert1",
+    "client-cert2"
+  ]
+}
+
+variable "host_project_id" {
+  type    = string
+  default = "test-vpc-host-tf12-sb"
+}
+
+variable "project_id" {
+  type    = string
+  default = "test-gke-fleet-member-tfc5-sb"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "test"
+}
