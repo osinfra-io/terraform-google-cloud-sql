c3681b971571c0ab73ef44e68adf03b9e3d18418
Brett Curtis <brett@osinfra.io>
2024-04-14 08:50:56 -0400

    Deprecation of Kitchen-Terraform (#45)

--- fixtures/default/main.tf
+++ fixtures/default/main.tf
@@ -1,5 +1,5 @@
 module "test" {
-  source = "../../../regional"
+  source = "../../regional"
 
   client_certs = var.client_certs
 
@@ -25,7 +25,7 @@ module "test" {
     team        = "testing"
   }
 
-  network                        = "kitchen-vpc"
+  network                        = "terraform-test-vpc"
   point_in_time_recovery_enabled = true
   project                        = var.project
   region                         = "us-east1"
