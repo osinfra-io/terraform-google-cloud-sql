a28dbae0af21732868d94a2ed205d0dcf284ec9e
Brett Curtis <brett@osinfra.io>
2024-04-14 08:22:45 -0400

    Fix test VPC name

--- fixtures/default/main.tf
+++ fixtures/default/main.tf
@@ -25,7 +25,7 @@ module "test" {
     team        = "testing"
   }
 
-  network                        = "kitchen-vpc"
+  network                        = "terraform-testing-vpc"
   point_in_time_recovery_enabled = true
   project                        = var.project
   region                         = "us-east1"
